a5d301454e8254681899f18e311935d850f324f5
tgrbtrfly <pagevpetty@live.com>
2019-05-05 01:14:29 -0600

    110    Add dummy data

--- app/src/main/java/com/notesapp/MainActivity.kt
+++ app/src/main/java/com/notesapp/MainActivity.kt
@@ -5,8 +5,16 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    var listNotes = ArrayList<Note>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add dummy data
+        listNotes.add(Note(1, "Meet Professor", "Note Description"))
+        listNotes.add(Note(2, "Meet Friend", "Note Description"))
+        listNotes.add(Note(3, "Meet Doctor", "Note Description"))
+
     }
 }
--- app/src/main/java/com/notesapp/Notes.kt
+++ app/src/main/java/com/notesapp/Notes.kt
@@ -1,2 +1,16 @@
 package com.notesapp
 
+class Note {
+
+    var noteID: Int? = null
+    var noteName: String? = null
+    var noteDes: String? = null
+
+    constructor(noteID: Int, noteName: String, noteDes: String) {
+        this.noteID = noteID
+        this.noteName = noteName
+        this.noteDes = noteDes
+    }
+
+}
+
