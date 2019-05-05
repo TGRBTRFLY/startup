413f7a3a54ef7d710e0e48c5832a311657cd7839
tgrbtrfly <pagevpetty@live.com>
2019-05-05 02:05:05 -0600

    110.1 List dummy notes use ListView

--- app/src/main/java/com/notesapp/MainActivity.kt
+++ app/src/main/java/com/notesapp/MainActivity.kt
@@ -2,6 +2,11 @@ package com.notesapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.ticket.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +21,38 @@ class MainActivity : AppCompatActivity() {
         listNotes.add(Note(2, "Meet Friend", "Note Description"))
         listNotes.add(Note(3, "Meet Doctor", "Note Description"))
 
+        var myNotesAdapter = MyNotesAdapter(listNotes)
+        notesLV.adapter = myNotesAdapter
+    }
+
+    inner class MyNotesAdapter : BaseAdapter {
+        var listNotesAdapter = ArrayList<Note>()
+
+        constructor(listNotesAdapter: ArrayList<Note>) : super() {
+            this.listNotesAdapter = listNotesAdapter
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var myView = layoutInflater.inflate(R.layout.ticket, null)
+            var myNote = listNotesAdapter[position]
+            myView.titleTV.text = myNote.noteName
+            myView.desTV.text = myNote.noteDes
+
+            return myView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listNotesAdapter[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listNotesAdapter.size
+        }
+
     }
 }
+
